fa684f0ae423ba4269d890429a96928fc2759cfc
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-30 16:10:09 +0200

    Extension finished

--- src/main/java/com/booleanuk/api/bagels/controllers/ProductsController.java
+++ src/main/java/com/booleanuk/api/bagels/controllers/ProductsController.java
@@ -4,8 +4,10 @@ import com.booleanuk.api.bagels.models.Product;
 import com.booleanuk.api.bagels.repositories.ProductsRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("products")
 public class ProductsController {
@@ -16,31 +18,65 @@ public class ProductsController {
     }
 
     @GetMapping
-    public List<Product> findAll(){
-        return this.theProducts.findAll();
+    public ArrayList<Product> findAll(@RequestParam (required = false) String category){
+        ArrayList<Product> categoryProductList = this.theProducts.findAll(category);
 
+        if (categoryProductList != null){
+            return categoryProductList;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No product of the provided category were found");
     }
 
     @GetMapping("{id}")
     public Product find(@PathVariable int id){
-        return this.theProducts.find(id);
+        Product product = this.theProducts.find(id);
+
+        if (product != null){
+            return product;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Product create(@RequestBody Product product){
-        return this.theProducts.create(product);
+        Product newProduct = this.theProducts.create(product);
+        if (newProduct != null){
+            return newProduct;
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product with provided name already exists");
+
     }
 
     @PutMapping("{id}")
     public Product update(@PathVariable int id, @RequestBody Product product){
-        return this.theProducts.update(id, product);
+        Product updatedProduct;
+
+        try {
+            updatedProduct = this.theProducts.update(id, product);
+        } catch (IllegalArgumentException e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product with the provided name already exists.");
+        }
+
+        if (updatedProduct != null){
+            return updatedProduct;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No product with that ID was found");
     }
 
     @DeleteMapping("{id}")
     public Product remove(@PathVariable int id){
-        return this.theProducts.remove(id);
-    }
+        Product removedProduct = this.theProducts.remove(id);
 
+        if (removedProduct != null){
+            return removedProduct;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+    }
 
 }
--- src/main/java/com/booleanuk/api/bagels/repositories/ProductsRepository.java
+++ src/main/java/com/booleanuk/api/bagels/repositories/ProductsRepository.java
@@ -1,15 +1,13 @@
 package com.booleanuk.api.bagels.repositories;
 
 import com.booleanuk.api.bagels.models.Product;
-import com.sun.source.tree.BreakTree;
+import org.springframework.web.server.ResponseStatusException;
 
-import javax.xml.crypto.Data;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class ProductsRepository {
-    private List<Product> data = new ArrayList<>();
+    private ArrayList<Product> data;
 
     public ProductsRepository(){
         this.data = new ArrayList<>();
@@ -32,7 +30,19 @@ public class ProductsRepository {
         return product;
     }
 
-    public List<Product> findAll() {
+    public ArrayList<Product> findAll(String category) {
+        ArrayList<Product> categoryList = new ArrayList<>();
+
+        for (Product product : data){
+            if(product.getCategory().equalsIgnoreCase(category)){
+                categoryList.add(product);
+            }
+        }
+
+        if (!categoryList.isEmpty()){
+            return categoryList;
+        }
+
         return this.data;
     }
 
@@ -46,7 +56,11 @@ public class ProductsRepository {
     }
 
     public Product update(int id, Product insertProduct){
+
         for(Product product : data){
+            if (product.getId() == id && Objects.equals(product.getName(), insertProduct.getName())) {
+                throw new IllegalArgumentException();
+            }
             if (product.getId() == id){
                 product.setName(insertProduct.getName());
                 product.setCategory(insertProduct.getCategory());
